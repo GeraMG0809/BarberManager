5c4a9d188bb57001ee35c012e089c9c1887d84e5
GeraMG0809 <gerardo.mercado5055@alumnos.udg.mx>
2025-03-12 14:44:42 -0600

    Registro de usuarios en la BD

--- helpers/conection.py
+++ helpers/conection.py
@@ -1,5 +1,4 @@
 import pymysql
 
 def Connection()-> pymysql.connections.Connection:
-    return pymysql.connect(host="localhost", user='root',password='1234',db='barberManager')
-
+    return pymysql.connect(host="localhost", user='root',password='gerardojr0809',db='barberManager')
--- helpers/user.py
+++ helpers/user.py
@@ -2,24 +2,26 @@ from helpers.conection import *
 from models.User_model import *
     
 
-def new_user(name:str,email:str,password:str,)-> bool: 
-    barberManager = Connection()
-
+def new_user(name: str, telefono: str, email: str, password: str) -> bool: 
+    barberManager = Connection()  # Suponiendo que esta función devuelve una conexión válida con pymysql
 
     with barberManager.cursor() as cursor:
-        cursor.execute(f"SELECT COUNT(*) FROM Usuario WHERE Correo_electronico = {email}")
+        cursor.execute("SELECT * FROM Usuario WHERE correo_electronico = %s", (email,))
         res = cursor.fetchone()
 
-    if res != (0,):
+    if res is not None:  # Si el usuario ya existe
         return False
     
-
     with barberManager.cursor() as cursor:
-        cursor.execute(f"INSERT INTO Usuario(nombre_usuario,correo_electronico,constraseña) VALUES (%s,%s,%s)",{name},{email},{password})
+        cursor.execute(
+            "INSERT INTO Usuario (nombre_usuario, telefono_usuario, correo_electronico, contraseña) VALUES (%s, %s, %s, %s)",
+            (name, telefono, email, password)  # 👈 Pasar los valores como una tupla
+        )
 
-    barberManager.commit()
-    barberManager.close()
+    barberManager.commit()  # Guardar los cambios
+    barberManager.close()  # Cerrar la conexión
     return True
+
 
 def select_user_id(id:int):
     barberManager = Connection()
