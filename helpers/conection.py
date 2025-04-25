a6eace9c88e8590e50fe32d116bd7f74abc19b2a
GeraMG0809 <gerardo.mercado5055@alumnos.udg.mx>
2025-04-25 02:31:32 -0600

    fix connection

--- helpers/conection.py
+++ helpers/conection.py
@@ -2,3 +2,16 @@ import pymysql
 
 def Connection()-> pymysql.connections.Connection:
     return pymysql.connect(host="localhost", user='root',password='gerardojr0809',db='barberManager')
+import pymysql
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Cargar variables del archivo .env
+
+def Connection() -> pymysql.connections.Connection:
+    return pymysql.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        db=os.getenv("DB_NAME")
+    )
